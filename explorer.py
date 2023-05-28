a42748318c0b385b7ee189dd7a985c399fe6f22e
niro <niro@vanti-analytics.com>
2023-05-28 14:27:06 +0300

    asdf

--- explorer.py
+++ explorer.py
@@ -7,6 +7,6 @@ def view_document(supabase):
     st.write("**This feature is in active development**")
     # Display a list of elements from the documents
     # If the user clicks on an element, display the content of the document
-    for document in response.data:
-        if st.button(document['content'][:50].replace("\n", " ")):
+    for idx, document in enumerate(response.data):
+        if st.button(document['content'][:50].replace("\n", " "), key=idx):
             continue
